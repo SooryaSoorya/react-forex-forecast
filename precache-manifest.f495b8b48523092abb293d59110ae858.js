59407f8596302c3402b7790a8edbbc17d37fc360
SooryaSudha <soorya.sudha@mnscorp.net>
2020-04-18 16:37:19 +0300

    Updates

---
+++ precache-manifest.f495b8b48523092abb293d59110ae858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5add80e89517610179398b6c50f1af49",
+    "url": "/SooryaSoorya/react-forex-forecast/index.html"
+  },
+  {
+    "revision": "a27c0b9e49c618e772f9",
+    "url": "/SooryaSoorya/react-forex-forecast/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "c08ed648e6344f936361",
+    "url": "/SooryaSoorya/react-forex-forecast/static/css/main.c952eef5.chunk.css"
+  },
+  {
+    "revision": "a27c0b9e49c618e772f9",
+    "url": "/SooryaSoorya/react-forex-forecast/static/js/2.49c44a08.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/SooryaSoorya/react-forex-forecast/static/js/2.49c44a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08ed648e6344f936361",
+    "url": "/SooryaSoorya/react-forex-forecast/static/js/main.8e5c4121.chunk.js"
+  },
+  {
+    "revision": "993ae7895d74c21e1658",
+    "url": "/SooryaSoorya/react-forex-forecast/static/js/runtime-main.744d624c.js"
+  }
+]);
